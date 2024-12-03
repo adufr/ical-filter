e65eebb7b18dca817c130280aa140e786305d771
Arthur Dufour <dufourarthur.perso@gmail.com>
2024-12-03 08:17:52 +0100

    feat: initial commit (mvp)

---
+++ components/Rule.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { type Rule, ruleActions, ruleFields, ruleTypes } from '~/types'
+
+defineEmits<{
+  (e: 'remove'): void
+}>()
+
+const rule = defineModel<Rule>({ required: true })
+</script>
+
+<template>
+  <div class="grid grid-cols-5">
+    <USelect v-model="rule.field" :items="Object.keys(ruleFields)" />
+    <USelect v-model="rule.type" :items="Object.keys(ruleTypes)" />
+    <UInput v-model="rule.value" />
+    <USelect v-model="rule.action" :items="Object.keys(ruleActions)" />
+    <UButton leading-icon="i-heroicons-trash" @click="$emit('remove')" />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+/* eslint-disable node/prefer-global/process */
+import pkgJson from './package.json'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@vueuse/nuxt', '@nuxthub/core', '@nuxtjs/plausible'],
+
+  css: ['~/assets/css/main.css'],
+
+  typescript: {
+    typeCheck: false,
+  },
+
+  runtimeConfig: {
+    public: {
+      nodeEnv: process.env.NODE_ENV,
+      appVersion: pkgJson.version,
+    },
+  },
+
+  plausible: {
+    domain: 'ical-filter.arthurdufour.dev',
+    apiHost: 'https://plausible.arthurdufour.dev',
+  },
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+})
