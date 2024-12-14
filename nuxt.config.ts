3b9386b974f9f03dcfbfb9ae2dd51a2c77be05af
Arthur Dufour <dufourarthur.perso@gmail.com>
2024-12-14 19:26:24 +0100

    chore: setup `nuxt-schema-org`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable node/prefer-global/process */
-import pkgJson from './package.json'
+import { defineOrganization } from 'nuxt-schema-org/schema'
 
+import pkgJson from './package.json'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -14,6 +15,7 @@ export default defineNuxtConfig({
     '@nuxt/test-utils/module',
     '@nuxtjs/robots',
     '@nuxtjs/sitemap',
+    'nuxt-schema-org',
   ],
 
   css: ['~/assets/css/main.css'],
@@ -44,13 +46,20 @@ export default defineNuxtConfig({
     },
   },
 
+  // SEO related packages
   site: {
     url: 'https://ical-filter.arthurdufour.dev',
     name: 'iCalFilter',
   },
-
   routeRules: {
     // Don't add any /secret/** URLs to the sitemap.xml
     '/edit/**': { robots: false },
+  },
+  schemaOrg: {
+    identity: defineOrganization({
+      // Basic Information
+      name: 'iCalFilter',
+      logo: '/logo.png',
+    }),
   },
 })
