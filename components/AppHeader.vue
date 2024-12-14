bb2b0fceff92791bb1782bdf05b66b6c6259430c
Arthur Dufour <dufourarthur.perso@gmail.com>
2024-12-14 19:49:25 +0100

    feat: improve header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -4,7 +4,7 @@ const appVersion = config.public.appVersion
 </script>
 
 <template>
-  <div class="h-24 md:h-32 px-5 md:px-52 sticky top-0 z-50 flex items-center justify-between">
+  <div class="h-16 bg-white/70 dark:bg-slate-900/70 backdrop-blur shrink-0 px-5 md:px-52 sticky top-0 z-50 flex items-center justify-between border-b border-neutral-100 dark:border-slate-800">
     <div class="flex items-center gap-2">
       <NuxtLink to="/">
         <h1 class="text-neutral-950 dark:text-neutral-50 text-lg md:text-2xl font-bold hover:underline">
